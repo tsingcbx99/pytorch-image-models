d00e350c72c06fe13a98917bf4be15c8eef30747
tsingcbx99 <57670068+tsingcbx99@users.noreply.github.com>
2021-12-27 11:45:38 +0800

    Update jsd.py

--- timm/loss/jsd.py
+++ timm/loss/jsd.py
@@ -14,6 +14,7 @@ class JsdCrossEntropy(nn.Module):
 
     Hacked together by / Copyright 2020 Ross Wightman
     """
+
     def __init__(self, num_splits=3, alpha=12, smoothing=0.1):
         super().__init__()
         self.num_splits = num_splits
@@ -34,6 +35,6 @@ class JsdCrossEntropy(nn.Module):
 
         # Clamp mixture distribution to avoid exploding KL divergence
         logp_mixture = torch.clamp(torch.stack(probs).mean(axis=0), 1e-7, 1).log()
-        loss += self.alpha * sum([F.kl_div(
+        loss = loss + self.alpha * sum([F.kl_div(
             logp_mixture, p_split, reduction='batchmean') for p_split in probs]) / len(probs)
         return loss
